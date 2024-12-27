179837edbf79c19121d50d506e034d1a2a676b0c
Stranger <95301553+IrannaSanjayKokatanur@users.noreply.github.com>
2024-12-27 17:37:13 +0530

    Revert "Commited"
    This reverts commit 2d83b1ed788dbf71dbb4d5a5588a46c5f375a867.

--- TodoApplication/src/main/java/com/iranna/TodoApplication/repos/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iranna.TodoApplication.repos;
-
-import com.iranna.TodoApplication.models.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
